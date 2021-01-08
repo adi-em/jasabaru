d9dc241ad807dd6f1e55c9aaad89ce5c21b26226
adi-em <adimurdiono@student.unu-jogja.ac.id>
2021-01-08 15:58:58 +0700

    upload

---
+++ adminapp/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.tampilguru, name='pengajar'),
+    path('<id>/detailguru', views.detailguru, name='gdetail'),
+    path('<id_guru>/editguru', views.editguru, name='editguru'),
+    path('<id>/deleteguru', views.deleteguru, name='gdelete'),
+    path('tampilmurid', views.tampilmurid, name='murid'),
+    path('<id>/detailmurid', views.detailmurid, name='mdetail'),
+    path('<id_murid>/editmurid', views.editmurid, name='editmurid'),
+    path('<id>/deletemurid', views.deletemurid, name='mdelete'),
+    
+
+]
